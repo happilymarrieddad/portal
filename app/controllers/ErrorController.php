d1c0a8c6720e6ccf2483136d33ee277d63375fbc
Nick Kotenberg <happilymarrieddad@yahoo.com>
2014-07-25 10:34:22 -0600

    testing error options

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -43,7 +43,20 @@ class ErrorController extends \BaseController {
 	 */
 	public function show($id)
 	{
-        return View::make('error.show')->with('id', $id, 'error tester...');
+        $name = '';
+
+        if (Auth::check())
+        {
+            $name = 'Signed in as <a href="user/show" class="navbar-link">' . (Auth::user()->first_name ?: Auth::user()->email) . '</a>';
+
+            $session = '<li><a href="logout">Logout</a></li>';
+        }
+        else
+        {
+            $session = '<li></li><li><a href="login">Login</a></li>';
+        }
+
+        return View::make('error.show')->with('id', $id, 'error tester...')->with('session', $session)->with('name', $name);
 	}
 
 
