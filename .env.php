17d55a2f4a8e88c6c3de41df62eebc77ec6aba44
Nick Kotenberg <happilymarrieddad@yahoo.com>
2014-07-29 11:54:16 -0600

    update

--- .env.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'DB_MAIN'     => 'main',
-    'DB_BUILDER'  => 'builder',
-    'DB_HOST'     => 'seg-fault-developer.cr3nazmupmxl.us-east-1.rds.amazonaws.com',
-    'DB_USERNAME' => 'segfault',
-    'DB_PASSWORD' => 'Wurger12'
-);
