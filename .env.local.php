a9d93c3a0ba69a58bcaa45b3ba45281b6cd70835
Nick Kotenberg <happilymarrieddad@yahoo.com>
2014-07-29 11:31:08 -0600

    Delete .env.local.php

--- .env.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'DB_USERNAME' => 'segfault',
-    'DB_PASSWORD' => 'Wurger12',
-    'DB_BUILDER'  => 'builder',
-    'DB_MAIN'     => 'main',
-    'DB_HOST'     => 'seg-fault-developer.cr3nazmupmxl.us-east-1.rds.amazonaws.com'
-);
