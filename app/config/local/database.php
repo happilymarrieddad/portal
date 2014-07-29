4ad2e918173938d88b56485cefc30247e11cd5d0
Nick Kotenberg <happilymarrieddad@yahoo.com>
2014-07-29 17:09:05 -0600

    finished builder prototype

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -20,16 +20,27 @@ return array(
 
 	'connections' => array(
 
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => getenv('DB_HOST'),
-			'database'  => getenv('DB_MAIN'),
-			'username'  => getenv('DB_USERNAME'),
-			'password'  => getenv('DB_PASSWORD'),
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'seg-fault-developer.cr3nazmupmxl.us-east-1.rds.amazonaws.com',
+            'database'  => 'main',
+            'username'  => 'segfault',
+            'password'  => 'Wurger12',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+
+        'builder' => array(
+            'driver'    => 'mysql',
+            'host'      => 'seg-fault-developer.cr3nazmupmxl.us-east-1.rds.amazonaws.com',
+            'database'  => 'builder',
+            'username'  => 'segfault',
+            'password'  => 'Wurger12',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
 
 		'pgsql' => array(
 			'driver'   => 'pgsql',
