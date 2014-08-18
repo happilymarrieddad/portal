a311c7c99b7de38974ab1c3b25323ade7bc0b3b4
Nick Kotenberg <happilymarrieddad@yahoo.com>
2014-08-18 09:33:45 -0600

    test

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'seg-fault-developer.cr3nazmupmxl.us-east-1.rds.amazonaws.com',
+			'host'      => 'localhost',
 			'database'  => 'main',
 			'username'  => 'segfault',
-			'password'  => 'Wurger12',
+			'password'  => 'wurger12',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
@@ -65,10 +65,10 @@ return array(
 
         'builder' => array(
             'driver'    => 'mysql',
-            'host'      => 'seg-fault-developer.cr3nazmupmxl.us-east-1.rds.amazonaws.com',
+            'host'      => 'localhost',
             'database'  => 'builder',
             'username'  => 'segfault',
-            'password'  => 'Wurger12',
+            'password'  => 'wurger12',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
