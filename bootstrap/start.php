8408fbaeb561aabf9f8547521ab260f17b72ea90
Nick Kotenberg <happilymarrieddad@yahoo.com>
2014-09-12 02:43:22 -0600

    added local

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local'      => array('Administrators-MacBook-Pro-2.local','administorsmbp2.pk5001z', 'nick-P-7811FX'),
+	'local'      => array('nicks-mbp.pk5001z','administorsmbp2.pk5001z', 'nick-P-7811FX'),
     'production' => array('ip-172-31-27-108')
 
 ));
